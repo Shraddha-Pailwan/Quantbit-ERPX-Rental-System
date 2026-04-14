63e7922a2370c12309bc64d2fffc7c773b97d27d
Shraddha-Pailwan <shraddhapailwan21@gmail.com>
2026-04-14 16:15:37 +0530

    first commit

---
+++ quantbit_erpx_rental_system/quantbit_erpx_rental_system/doctype/owner_payout_entry/owner_payout_entry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Quantbit Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Owner Payout Entry", {
+// 	refresh(frm) {
+
+// 	},
+// });
